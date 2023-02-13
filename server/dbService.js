de8701ac5d13b0aea5f66284c83ce0f61c239b52
SprigganCS <andrenmed@hotmail.com>
2023-02-12 23:23:34 -0300

    undo var change

--- server/dbService.js
+++ server/dbService.js
@@ -3,11 +3,11 @@ const dotenv = require('dotenv'); // Dotenv para variaveis de ambiente
 dotenv.config(); //permite o acesso as credencias do .env
 
 const connection = mysql.createConnection({
-    host: process.env.MYSQLHOST,
-    user: process.env.MYSQLUSER,
-    password: process.env.MYSQLPASSWORD,
-    database: process.env.MYSQLDATABASE,
-    port: process.env.MYSQLPORT
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    port: process.env.DB_PORT
 });
 
 connection.connect((err) => {
