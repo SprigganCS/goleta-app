0b10563b318af65b1031653aade1efa73da0d1be
SprigganCS <andrenmed@hotmail.com>
2023-02-18 02:20:11 -0300

    fix: sql statement after working with dbms

--- server/dbService.js
+++ server/dbService.js
@@ -28,7 +28,7 @@ class DbService {
     try{
       const response = await new Promise((resolve, reject) => 
       {
-        const query = "SELECT * FROM tbl_passageiros WHERE id = 1;";
+        const query = "SELECT * FROM tbl_passageiros WHERE id_passageiro = 1;";
 
         connection.query(query, [1],(err, results) => 
         {
