eeddee109c098d7bc758679c7faa1e837d265a60
SprigganCS <andrenmed@hotmail.com>
2023-02-28 21:51:54 -0300

    frontend updates from weekend

--- client/goleta-react-native/src/components/Screens/Profile.js
+++ client/goleta-react-native/src/components/Screens/Profile.js
@@ -1,13 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 const Profile = () =>{
     return (
         <View style={styles.container}>
-            <Text style={styles.text}>Profile</Text>
+            <Text style={styles.text}>SprigganCS</Text>
+
+            <View style={styles.box}>
+                <Text style={styles.data}>First Middle Last_name</Text>
+                <View style={styles.btnContainer}>
+                    <TouchableOpacity>
+                        <Text style={{fontSize: 15}}>Editar</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+
+            <View style={styles.box}>
+                <Text style={styles.data}>+BR DDD XXXXX-XXXX</Text>
+                <View style={styles.btnContainer}>
+                    <TouchableOpacity>
+                        <Text style={{fontSize: 15}}>Editar</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+
+            <View style={styles.box}>
+                <Text style={styles.data}>first.last@domain.something.com.br</Text>
+                <View style={styles.btnContainer}>
+                    <TouchableOpacity>
+                        <Text style={{fontSize: 15}}>Editar</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+
+            <View style={styles.box}>
+                <Text style={styles.data}>************</Text>
+                <View style={styles.btnContainer}>
+                    <TouchableOpacity>
+                        <Text style={{fontSize: 15}}>Editar</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+
+            
             <StatusBar style="auto" />
         </View>
-);
+    );
 }
 
 const styles = StyleSheet.create({
@@ -20,6 +58,36 @@ const styles = StyleSheet.create({
         color: 'black',
         textAlign: 'center',
         marginTop: 50,
+        marginBottom: 10,
+    },
+    box :{
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginTop: 10,
+        marginBottom: 10,
+        
+    },
+    data:{
+        width: '80%',
+        fontSize: 15,
+        marginLeft: 7,
+        backgroundColor: 'darkgrey',
+        borderRadius: 10,
+        textAlign: 'center',
+        textAlignVertical: 'center',
+        height: 40,
+    },
+    btnContainer: {
+        marginRight: 15,
+        marginLeft: 5,
+        alignItems: 'center',
+        width: '15%',
+        backgroundColor: '#610c0c',
+        borderRadius: 10,
+        height: 40,
+
+        justifyContent: 'center',
+
     },
 });
 
--- client/goleta-react-native/src/components/Screens/Schedule.js
+++ client/goleta-react-native/src/components/Screens/Schedule.js
@@ -1,8 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { useState } from 'react';
+import { ActivityIndicator, SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { useState, useEffect } from 'react';
 
-const Schedule =() =>{
+const Schedule = () => {
     const today = new Date();
     const day = today.getDay();
     const dd = parseInt(String(today.getDate()).padStart(2, '0'));
@@ -12,14 +12,47 @@ const Schedule =() =>{
 
     const [shouldShow, setshouldShow] = useState(false);
 
+
+    let [isLoading, setIsLoading] = useState(true);
+    let [error, setError] = useState(null);
+    let [response, setResponse] = useState(null);
+
+    useEffect(() => {
+        fetch('https://goleta-app-production.up.railway.app/users')
+            .then((res) => res.json())
+            .then(
+                (result) => {
+                    setIsLoading(false);
+                    setResponse(result)
+                },
+
+                (error) => {
+                    setIsLoading(false);
+                    setError(error);
+                }
+            )
+    }, []);
+
+    const getTime = (i) => {
+        if (isLoading) {
+            return <ActivityIndicator size="small" />;
+        }
+
+        if (error) {
+            return <Text>{error}</Text>;
+        }
+        console.log(response.data[i].nome_passageiro);
+        return <Text style={{ textAlign: 'center' }}>{response.data[i].nome_passageiro}</Text>;
+    }
+
     return (
         <View style={styles.container}>
             <View style={styles.box}>
                 <View style={styles.inner}>
                     <TouchableOpacity
-                    hitSlop= {{top: 30, right: 30, left: 30, bottom: 30}}
-                    onPress={() => setshouldShow(!shouldShow)}>
-                        <Text style={styles.date}>{(dd) +'/'+mm}</Text>
+                        hitSlop={{ top: 30, right: 30, left: 30, bottom: 30 }}
+                        onPress={() => setshouldShow(!shouldShow)}>
+                        <Text style={styles.date}>{(dd) + '/' + mm}</Text>
                         <Text>{dias[day]}</Text>
                     </TouchableOpacity>
                 </View>
@@ -27,104 +60,121 @@ const Schedule =() =>{
 
             <View style={styles.box}>
                 <View style={styles.inner}>
-                    <Text style={styles.date}>{(dd+1) +'/'+mm}</Text>
-                    <Text>{dias[(day+1)%7]}</Text>
+                    <Text style={styles.date}>{(dd + 1) + '/' + mm}</Text>
+                    <Text>{dias[(day + 1) % 7]}</Text>
                 </View>
             </View>
 
             <View style={styles.box}>
                 <View style={styles.inner}>
-                    <Text style={styles.date}>{(dd+2) +'/'+mm}</Text>
-                    <Text>{dias[(day+2)%7]}</Text>
+                    <Text style={styles.date}>{(dd + 2) + '/' + mm}</Text>
+                    <Text>{dias[(day + 2) % 7]}</Text>
                 </View>
             </View>
 
             <View style={styles.box}>
                 <View style={styles.inner}>
-                    <Text style={styles.date}>{(dd+3) +'/'+mm}</Text>
-                    <Text>{dias[(day+3)%7]}</Text>
+                    <Text style={styles.date}>{(dd + 3) + '/' + mm}</Text>
+                    <Text>{dias[(day + 3) % 7]}</Text>
                 </View>
             </View>
 
             <View style={styles.box}>
                 <View style={styles.inner}>
-                    <Text style={styles.date}>{(dd+4) +'/'+mm}</Text>
-                    <Text>{dias[(day+4)%7]}</Text>
+                    <Text style={styles.date}>{(dd + 4) + '/' + mm}</Text>
+                    <Text>{dias[(day + 4) % 7]}</Text>
                 </View>
             </View>
 
-            
-            
+
+
 
 
             <SafeAreaView>
-               
+
                 {
                     shouldShow ? (
-                        <View style = {styles.hiddenContainer}>
-                            <View style={styles.col}>
-                                <Text style={{textAlign:'center'}}>Ida</Text>
+                        <View style = {styles.tab}>
+                            <View style={styles.hiddenContainer}>
+                                <View style={styles.lin}>
+
+                                    {/* {getTime(1)} */}
+                                    <Text style={{ textAlign: 'center', marginLeft: 10 }}>Ida</Text>
+
+
+                                </View>
+                                <View style={styles.lin}>
+                                    <Text style={{ textAlign: 'center', marginLeft: 10 }}>Volta</Text>
+                                </View>
+
                             </View>
-                            <View style={styles.col}>
-                                <Text style={{textAlign:'center'}}>Volta</Text>
-                            </View>
-                            
                         </View>
-                            
-                    ) :null
+
+
+                    ) : null
                 }
-                
+
             </SafeAreaView>
-            
+
             <StatusBar style="auto" />
         </View>
 
-        
-        
+
+
     );
 }
 
 
 const styles = StyleSheet.create({
     container: {
-    width: '100%',
-    height: '100%',
-    flexDirection: 'row',
-    justifyContent: 'space-evenly',
-    backgroundColor: '#fff',
-    flexWrap: 'wrap',
-  },
-  box: {
-    width: "20%",
-    height: 60,
-    padding: 5,
-  },
-  inner: {
-    flex: 1,
-    backgroundColor: '#eee',
-    alignItems: 'center', //horizontal
-    justifyContent: 'center', //vertical
-    borderRadius: 5,
-  },
-  date: {
-    fontSize: 10,
-  },
-  hideshow:{ 
-    width: '100%',
-    height: '100%',
-  },
-  hiddenContainer:{
-    width: '100%',
-    height: '100%',
-    flexDirection: 'row',
-    justifyContent: 'space-evenly',
-  },
-  col:{
-    width: "20%",
-    height: '5%',
-    padding: 5,
-    backgroundColor: '#bbb',
-  }
+        width: '100%',
+        height: '100%',
+        flexDirection: 'row',
+        justifyContent: 'space-evenly',
+        backgroundColor: '#fff',
+        flexWrap: 'wrap',
+    },
+    box: {
+        width: "20%",
+        height: 60,
+        padding: 5,
+    },
+    inner: {
+        flex: 1,
+        backgroundColor: '#eee',
+        alignItems: 'center', //horizontal
+        justifyContent: 'center', //vertical
+        borderRadius: 5,
+    },
+    date: {
+        fontSize: 10,
+    },
+    tab: {
+        marginTop: 30,
+        width: '100%',
+        height: '100%',
+        //backgroundColor: 'yellow',
+        alignItems: 'center',
+        
+    },
+    hiddenContainer: {
+        width: '80%',
+        height: '30%',
+        flexDirection: 'row',
+        backgroundColor: '#eee',
+        borderRadius: 10,
+        flexWrap: 'wrap',
+        justifyContent: 'space-evenly',
+    },
+    lin: {
+        width: "100%",
+        height: '20%',
+        marginBottom: 10,
+        marginTop: 37,
+        //backgroundColor: 'blue',
+        justifyContent: 'center',
+        alignItems: 'flex-start',
+    }
 });
 
 
